863c54a67207d18837a3cf74374e61a396b110a9
Dmitry <mit0787@gmail.com>
2019-04-18 17:18:06 +0300

    Первый урок
    Выполнил первый обязательный урок

---
+++ lesson_1/js/script.js
@@ -0,0 +1,24 @@
+'use strict';
+
+let money = +prompt("Ваш бюджет на месяц?", ""),
+    time = prompt("Введите дату в формате YYYY-MM-DD", ""),
+    appData = {
+      budget: money,
+      timeData: time,
+      expenses: {},
+      optionalExpenses: '',
+      income: [],
+      savings: false
+    },
+    expenseOne = prompt("Введите обязательную статью расходов в этом месяце", ""),
+    costOne = +prompt("Во сколько обойдется?", ""),
+    expenseTwo = prompt("Введите обязательную статью расходов в этом месяце", ""),
+    costTwo = +prompt("Во сколько обойдется?", ""),
+    expenseSum = 0;
+appData.expenses[expenseOne] = costOne;
+appData.expenses[expenseTwo] = costTwo;
+for (let name in appData.expenses) {
+  expenseSum += appData.expenses[name];
+}
+let budgetPerDay = (appData.budget - expenseSum) / 30;
+alert(Math.floor(budgetPerDay));
